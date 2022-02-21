f81172fcc6415ceeb5c923e4ec83ce9bce8e4fd5
Boris Kabakchiev <92682426+bobikab@users.noreply.github.com>
2022-02-21 20:29:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Helo world')
+import tkinter as tk
+
+if __name__ == '__main__':
+    window = tk.Tk()#tova e samata vizializaciq na prozoreca
+    window.geometry('700x700')#razmerite
+    window.title('My GUI Shop')#tova e kakvo da pishe vmesto tk
+    window.mainloop()#staratira
