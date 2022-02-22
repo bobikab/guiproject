50cdc111ca058eec5178b7224c22fd521460b2e5
Boris Kabakchiev <92682426+bobikab@users.noreply.github.com>
2022-02-22 16:17:22 +0200

    adding buttons and some operation adding new file and putting usenrames and so on in json dict added login and rehister operation #putting funcs in files and making the image components

--- products_service.py
+++ products_service.py
@@ -1,5 +1,5 @@
 import json
 def get_all_products():
     with open('db/products.txt','r') as file:
-       return [json.loads(x.strip()) for x in file]
+       return [json.loads(x.strip()) for x in file]#
 
