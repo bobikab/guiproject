3a7a99b8d216fe7b07626ce76e46fe837c3d1393
Boris Kabakchiev <92682426+bobikab@users.noreply.github.com>
2022-02-22 14:56:50 +0200

    adding buttons and some operation adding new file and putting usenrames and so on in json dict added login and rehister operation

--- auth_service.py
+++ auth_service.py
@@ -7,14 +7,24 @@ def register(username,email,password):
         'password':password
     }
 
-    user_json = json.dumps(user_obj)
-    with open('./db/users.txt','a') as file:
+    user_json = json.dumps(user_obj)#tuka mi go dava kato stringov dict
+    with open('users.txt','r+') as file:
+        for user_line in file:#Validating if the user is in the user  if not add else not
+            existing_user = json.loads(user_line.strip())
+            if existing_user['username'] == username:
+                return False
+
         file.write(user_json + '\n')
+        return True
 
+def login(username,password):
 
-
-
-
+    with open('users.txt','r+') as file:
+        for user_line in file:
+            existing_user = json.loads(user_line.strip())
+            if existing_user['username'] == username and existing_user['password'] == password:
+                return True
+        return False
 
 
 
